e514ef67eef3a1ac1c45f9dcaf5d3c2ee4159e38
begidudimtse <begidudimtse@gmail.com>
2025-10-06 19:05:07 +0300

    Implementing a share layout

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/layouts/Layout";
+
+
+
+
+export default function Home() {
+  return (
+   <Layout>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      </main>
+    </Layout>
+  );
+}
